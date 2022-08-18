aa374e193b405fb987b360f7985ce005d6417877
Emily Mitcheson-Smith <106613906+emmymitch@users.noreply.github.com>
2022-08-18 10:41:52 +0100

    Getters and setters

--- src/main/java/org/example/Card.java
+++ src/main/java/org/example/Card.java
@@ -12,6 +12,30 @@ public class Card {
         this.value = value;
     }
 
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
     @Override
     public String toString() {
         return symbol + suit;
